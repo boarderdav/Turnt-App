dbfff1c2adaebb36e14c2f1aaa370032f0f343cf
Jake Spracher <tpspracher@MacBook-Pro.local>
2014-12-31 18:27:18 -0500

    Initial Commit

---
+++ Turnt_1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Turnt_1
+//
+//  Created by Jake Spracher on 12/31/14.
+//  Copyright (c) 2014 Turnt Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
